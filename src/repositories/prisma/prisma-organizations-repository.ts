506930613d7aff130e4f053df4a6222955492f22
Felipe Bastos <felipe.bastos.dev@gmail.com>
2023-10-05 10:27:16 -0300

    feat: create connection with db and fix organizations repository

--- src/repositories/prisma/prisma-organizations-repository.ts
+++ src/repositories/prisma/prisma-organizations-repository.ts
@@ -1,3 +1,12 @@
+import { Organization, Prisma } from '@prisma/client'
+
+import { prisma } from '@/lib/prisma'
 import { OrganizationsRepository } from '../organizations-repository'
 
-export class PrismaOrganizationsRepository implements OrganizationsRepository {}
+export class PrismaOrganizationsRepository implements OrganizationsRepository {
+  async create(data: Prisma.OrganizationCreateInput): Promise<Organization> {
+    const organization = await prisma.organization.create({ data })
+
+    return organization
+  }
+}
